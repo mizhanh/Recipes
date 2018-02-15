d18d166462998f896ae7d0af1d63071c2e127264
wollenet <ewollenberg16@jcu.edu>
2018-02-14 20:46:00 -0500

    db

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE recipes_db;
+USE recipes_db;
+
+CREATE TABLE recipes 
+(
+	id int NOT NULL AUTO_INCREMENT,
+	title varchar(30) NOT NULL,
+    category
+    author varchar(40) NOT NULL,
+    URL
+    description varchar (400) NOT NULL,
+	comments varchar (400) NOT NULL,
+    preparationTime integer (
+    
+    
+  	PRIMARY KEY(id)
+);
