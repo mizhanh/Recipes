4ece415397f52f348606c76458b8bf61a29e2f1b
Good Beard <goodbeard@Goods-MacBook-Pro.local>
2018-02-17 11:08:43 -0500

    still working on POST, but working Recipe model.

---
+++ models/recipe.js
@@ -0,0 +1,45 @@
+// Dependencies
+// =============================================================
+
+// // Sequelize (capital) references the standard library
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references our connection to the DB.
+// var sequelize = require("../config/connection.js");
+
+module.exports = function(sequelize, DataTypes) {
+  // Creates a "Recipe" model that matches up with DB
+  var Recipe = sequelize.define("recipe", {
+    // the routeName gets saved as a string
+    title: DataTypes.STRING,
+    // the name of the recipe (a string)
+    category: DataTypes.STRING,
+    // the category (a string for now, possible switch later)
+    author: DataTypes.STRING,
+    // the author (a string)
+    URL: DataTypes.STRING,
+    // and links (a string)
+    description: DataTypes.STRING,
+    // description (a string)
+    ingredients: DataTypes.JSON,
+    // ingredients (maybe more appropriate to use .JSON?  store ingredients as a JSON object?)
+    directions: DataTypes.TEXT,
+    // directions (a text for additional space)
+    comments: DataTypes.TEXT,
+    // comments (a text for additional space)
+    preparation_time: DataTypes.INTEGER,
+    // prep time (integer)
+    servings: DataTypes.INTEGER,
+    //  servings (integer)
+    difficulty: DataTypes.STRING
+    // there may be a better way to store this
+  }, {
+    timestamps: false
+  });
+  return Recipe;
+}
+
+// // Syncs with DB
+// Recipe.sync();
+
+// // Makes the Recipe Model available for other files (will also create a table)
+// module.exports = Recipe;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@
 
 // Dependencies
 // =============================================================
-
+// var Recipe = require("../models/recipe.js");
 // Requiring our Todo model
 var db = require("../models");
 
@@ -13,9 +13,33 @@ var db = require("../models");
 module.exports = function(app) {
 
   // GET route for getting all of the recipes
-  app.get("/api/posts/", function(req, res) {
+  app.get("/api/all/", function(req, res) {
     db.Post.findAll({})
     .then(function(dbPost) {
       res.json(dbPost);
     });
   });
+
+
+  app.post("/api/new", function(req, res) {
+  	var recipe = req.body;
+  	console.log(recipe);
+  	db.Recipe.create(req.body)
+  	// db.Recipe.create({
+  	// 	title: recipe.title,
+  	// 	category: recipe.category,
+  	// 	author: recipe.author,
+  	// 	URL: recipe.URL,
+  	// 	description: recipe.description,
+  	// 	ingredients: recipe.ingredients,
+  	// 	directions: recipe.directions,
+  	// 	comments: recipe.comments,
+  	// 	preparation_time: recipe.preparation_time,
+  	// 	servings: recipe.servings,
+  	// 	difficulty: recipe.difficulty
+  	// })
+  	.then(function(dbRecipe) {
+  		res.json(dbRecipe);
+  	});
+  });
+};
