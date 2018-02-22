2d20229e03a16c00514aeb992b3515c8c19864c0
mizhanh <31321795+mizhanh@users.noreply.github.com>
2018-02-22 11:24:37 -0500

    fine tuning the UI portion of the recipes through modification of my-recipes.js and homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -76,16 +76,16 @@ function renderRecipes(data) {
       
       div.append('<h2 style="font-size:30px; color:#4CAE4C; font-weight:bold">' + data[i].title + '</h2>');
       div.append('<img src="' + data[i].URL + '" style="width:200px; height:200px;"/>');
-      div.append('<p style="font-size:16px">Category: ' + data[i].category + '</p>');
-      div.append('<p style="font-size:16px">Author: ' + data[i].author + '</p>');
-      div.append('<p style="font-size:16px">Description: ' + data[i].description + '</p>');
-      div.append('<p style="font-size:16px">Ingredients: ' + data[i].ingredients + '</p>');
-      div.append('<p style="font-size:16px">Directions: ' + data[i].directions + '</p>');
-      div.append('<p style="font-size:16px">Comment: ' + data[i].comment + '</p>');
-      div.append('<p style="font-size:16px">Preparation Time: ' + data[i].preparation_time + '</p>');
-      div.append('<p style="font-size:16px">Servings: ' + data[i].servings + '</p>');
-      div.append('<p style="font-size:16px">Difficulty: ' + data[i].difficulty + '</p>');
-      div.append('<p style="font-size:16px">Favorite: ' + data[i].favorite + '</p');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Category: </span>' + data[i].category + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Author: </span>' + data[i].author + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Description: </span>' + data[i].description + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Ingredients: </span>' + data[i].ingredients + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Directions: </span>' + data[i].directions + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Comment: </span>' + data[i].comment + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Preparation Time: </span>' + data[i].preparation_time + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Servings: </span>' + data[i].servings + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Difficulty: </span>' + data[i].difficulty + '</p>');
+      div.append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Favorite: </span>' + data[i].favorite + '</p');
 
       if (data[i].favorite !== true) {
           div.append("<button style='background-color:#5cb85c; color:#fff; padding:6px 12px; font-size:14px; text-line:center; border-radius:8px; font-weight:400; border:2px solid #4CAE4C;' class='addFav' data-id='" + data[i].id + "'>Add My Favorite</button>");
--- public/js/my-recipes.js
+++ public/js/my-recipes.js
@@ -8,25 +8,25 @@ $.get("/api/all/favorite", function(data) {
     var myRecipes = $("<div>");
     // Add a class to this div: 'well'
     myRecipes.addClass("well");
-  //   // Add an id to the well to mark which well it is
+    // Add an id to the well to mark which well it is
     myRecipes.attr("id", "My_Recipe_" + i);
-  //   // Append the well to the well section
+    // Append the well to the well section
     $("#show-submit-recipe").append(myRecipes);
 
-  //   // Now  we add our recipe data to the well we just placed on the page
+    // Now  we add our recipe data to the well we just placed on the page
     $("#My_Recipe_" + i).append('<h2 style="font-size:30px; color:#4CAE4C; font-weight:bold; margin-top:0px;">' + data[i].title + '</h2>');
     $("#My_Recipe_" + i).append('<img src="' + data[i].URL + '" style="width:200px; height:200px;"/>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Category: ' + data[i].category + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Author: ' + data[i].author + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Category: </span>' + data[i].category + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Author: </span>' + data[i].author + '</p>');
     
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Description: ' + data[i].description + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Ingredients: ' + data[i].ingredients + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Directions: ' + data[i].directions + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Comment: ' + data[i].comment + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Preparation Time: ' + data[i].preparation_time + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Servings: ' + data[i].servings + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Difficulty: ' + data[i].difficulty + '</p>');
-    $("#My_Recipe_" + i).append('<p style="font-size:16px">Favorite: ' + data[i].favorite + '</p');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Description: </span>' + data[i].description + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Ingredients: </span>' + data[i].ingredients + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Directions: </span>' + data[i].directions + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Comment: </span>' + data[i].comment + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Preparation Time: </span>' + data[i].preparation_time + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Servings: </span>' + data[i].servings + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Difficulty: </span>' + data[i].difficulty + '</p>');
+    $("#My_Recipe_" + i).append('<p style="font-size:16px">' + '<span style="font-weight:bold; margin-right:5px;">Favorite: </span>' + data[i].favorite + '</p');
   }
 });
 
